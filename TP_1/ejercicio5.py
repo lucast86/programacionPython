fc569b17505115d3a131fed91a306d35e4657620
Lucas Trento <lucastrento86@gmail.com>
2022-03-25 01:42:16 -0300

    ejerciocio5Terminado

--- TP_1/ejercicio5.py
+++ TP_1/ejercicio5.py
@@ -1,3 +1,11 @@
 #ejercicio5 - Lucas Trento
 """Escribe un programa que pida 3 números e imprima el mayor de los tres.."""
 
+numeros = []
+cont = 0
+for cont in range(3):
+            numeros.append(int(input("Ingrese un numero: ")))
+            cont += 1
+
+numeros.sort()
+print("el mayor es: ", numeros.pop(2))
