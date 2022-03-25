3be4e489f45094288a4af426d0c360dc96a89459
Lucas Trento <lucastrento86@gmail.com>
2022-03-25 01:18:48 -0300

    ejercicio2Terminado

--- TP_1/ejercicio2.py
+++ TP_1/ejercicio2.py
@@ -1,3 +1,9 @@
 #ejercicio2 - Lucas Trento
 """El programa debe calcula el área de un triángulo. El usuario debe ingresar cada una de las
 medidas de sus lados."""
+
+trianguloAltura = float(input("Ingrese la altura del angulo : "))
+trianguloBase = float(input("Ingrese la base del angulo : "))
+
+areaTriangulo = (trianguloAltura * trianguloBase) / 2
+print(f"El area del trenagulo es : {areaTriangulo} ")
